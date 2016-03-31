0bf699992c144ad4de5f59dd1ea919162eda308a
dolceficante <john.dolce@MacBookAir-johndolce.local>
2016-03-31 16:29:25 -0500

    reverting a mistake

--- bundle/src/main/java/com/acquia/connectors/impl/ContentHubReplicationListener.java
+++ bundle/src/main/java/com/acquia/connectors/impl/ContentHubReplicationListener.java
@@ -45,6 +45,7 @@ public class ContentHubReplicationListener implements Runnable, EventHandler {
 	
 	private BundleContext bundleContext;
 	
+	  //Inject a Sling ResourceResolverFactory
     @Reference
     private ResourceResolverFactory resolverFactory;
     
@@ -54,13 +55,20 @@ public class ContentHubReplicationListener implements Runnable, EventHandler {
      
     private ObservationManager observationManager;
      
+  //Inject a Sling ResourceResolverFactory to create a Session requited by the EventHandler
     @Reference
     private SlingRepository repository;;	
 
 	public void handleEvent(Event eventData) {
+		LOG.debug("--handleEvent1");
 		ReplicationEvent event = ReplicationEvent.fromEvent(eventData);
+		LOG.debug("--handleEvent2");
 		ReplicationActionType type = event.getReplicationAction().getType();
-		LOG.debug("handleEvent type: " + type);
+		LOG.debug("--type: " + type);
+		String[] propertyNames = eventData.getPropertyNames();
+		for (int x=0; x<propertyNames.length; x++){
+			LOG.debug(">>propertyName: " + propertyNames[x]);
+		}
 		
 		ReplicationAction action = event.getReplicationAction();
 		String path = "";
@@ -81,7 +89,10 @@ public class ContentHubReplicationListener implements Runnable, EventHandler {
 		
 		LOG.debug("path = " + path);
 		Resource resource = resourceResolver.getResource(path);
+		
+
 		InheritanceValueMap pageProperties = new HierarchyNodeInheritanceValueMap(resource);
+		
 		LOG.debug("pageProperties.size() " + pageProperties.size());
 		
 		//Get configuration from cloud service configurations
@@ -103,10 +114,11 @@ public class ContentHubReplicationListener implements Runnable, EventHandler {
 	    LOG.debug("baseUrl: " + baseUrl);
 	    LOG.debug("origin: " + origin);
 	    LOG.debug("hostDomain: " + hostDomain);
-		
+				
 		
 		Map<String,String> config = new HashMap<String,String>();
 		config.put(ContentHubService.BASE_URL, baseUrl);
+
 		String resourceUrl = hostDomain + pathWithExt;
 		LOG.debug("resourceUrl: " + resourceUrl);
 		
@@ -117,6 +129,7 @@ public class ContentHubReplicationListener implements Runnable, EventHandler {
 					JSONObject result = postData(api, secret, origin, config, resourceUrl);
 					LOG.debug(result.toString());
 				} catch (Exception e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 					LOG.error("ERROR: " + e.getMessage());
 				}
@@ -128,14 +141,16 @@ public class ContentHubReplicationListener implements Runnable, EventHandler {
 	}
 	
 	private JSONObject postData(String api, String secret, String origin, Map<String,String> config, String resourceUrl) throws Exception {
-
+		// TODO Auto-generated method stub
 		ContentHubService service = ContentHubFactory.getInstance();
 		service.init(api, secret, origin, config);
 		JSONObject result = service.createEntities(resourceUrl);
+		//JSONObject result = service.settings();
 		return result;
 	}
 
 	public void run() {
+		// TODO Auto-generated method stub
 		LOG.info("ContentHubReplicationListener RUNNING!");
 	}
 
@@ -144,6 +159,7 @@ public class ContentHubReplicationListener implements Runnable, EventHandler {
 		try {
 			this.resourceResolver = resolverFactory.getAdministrativeResourceResolver(null);
 		} catch (LoginException e) {
+			// TODO Auto-generated catch block
 			LOG.info("ERROR" + e);
 			e.printStackTrace();
 		}
